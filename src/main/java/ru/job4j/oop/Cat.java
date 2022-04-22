30d5e5cfcef124ee1d630220a301e939f3bf1ed0
SpS <shchaaas@mail.ru>
2022-04-22 20:46:55 +0300

    1.5. Поля объекта [#166083]

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,27 @@
+package ru.job4j.oop;
+
+public class Cat {
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println("There are " + this.name + "'s food. " + this.food);
+    }
+    public void eat(String meat){
+        this.food=meat;
+    }
+    public void givNick(String nick){
+        this.name=nick;
+    }
+
+    public static void main(String[] args) {
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.name="Gav";
+        gav.show();
+        Cat black = new Cat();
+        black.eat("fish");
+        black.name="Black";
+        black.show();
+    }
+}
