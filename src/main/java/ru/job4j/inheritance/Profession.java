a0a670e9d8a96f8d98cc3e30270ac5f18564fa28
SpS <shchaaas@mail.ru>
2022-04-24 20:49:46 +0300

    4. Переопределение v3

--- src/main/java/ru/job4j/inheritance/Profession.java
+++ src/main/java/ru/job4j/inheritance/Profession.java
@@ -3,7 +3,7 @@ package ru.job4j.inheritance;
 public class Profession {
     private String name, surname, education, birthday;
 
-    public Profession (String name, String surname, String education, String birthday) {
+    public Profession(String name, String surname, String education, String birthday) {
         this.name = name;
         this.surname = surname;
         this.education = education;
