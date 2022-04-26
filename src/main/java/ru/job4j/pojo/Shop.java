8ad7175c4da12098cff57cc2e6addcaaf883d830
SpS <shchaaas@mail.ru>
2022-04-26 21:23:44 +0300

    3.2. Удаление моделей из массива. v3

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -2,7 +2,6 @@ package ru.job4j.pojo;
 
 public class Shop {
     public static int indexOfNull(Product[] products) {
-
         for (int i = 0; i < products.length; i++) {
             if (products[i] == null) {
                 return i;
