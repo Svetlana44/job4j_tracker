bbad7f942ee4c2904693967ddb5c2f81c868c3f0
SpS <shchaaas@mail.ru>
2022-04-26 21:22:19 +0300

    3.1. Массив с пустыми ячейками v2

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -4,7 +4,7 @@ public class Shop {
     public static int indexOfNull(Product[] products) {
 
         for (int i = 0; i < products.length; i++) {
-            if (products[i]==null) {
+            if (products[i] == null) {
                 return i;
             }
         }
