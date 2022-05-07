a26a997f2b83620e347c0aa87fa4bb07db6f982b
SpS <shchaaas@mail.ru>
2022-05-07 19:15:38 +0300

    5. Input и полиморфизм. [#181094]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -16,4 +16,15 @@ public class StartUITest {
         Item expected = new Item("Fix PC");
         assertEquals(created.getName(), expected.getName());
     }
+
+    @Test
+    public void whenCreateItem() {
+        String[] answers = {"Fix PC"};
+        Input input = new StubInput(answers);
+        Tracker tracker = new Tracker();
+        StartUI.createItem(input, tracker);
+        Item created = tracker.findAll()[0];
+        Item expected = new Item("Fix PC");
+        assertEquals(created.getName(), expected.getName());
+    }
 }
