f6c528e89df169a2c1e5f04f4ffaf2e0d1e9c76d
SpS <shchaaas@mail.ru>
2022-05-10 19:33:42 +0300

    9. Написать тесты на StartUI. [#785]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -10,25 +10,32 @@ public class StartUITest {
     @Test
     public void whenEditItem() {
         Tracker tracker = new Tracker();
-        Item item = new Item("new item");
-        tracker.add(item);
-        String[] answers = {String.valueOf(item.getId()), "edited item"};
-        ReplaceAction replaceAction = new ReplaceAction();
-        replaceAction.execute(new StubInput(answers), tracker);
-        Item edited = tracker.findById(item.getId());
-        assertEquals(edited.getName(), "edited item");
+        Item item = tracker.add(new Item("Replaced item"));
+        String replaced = "Item name edited";
+        Input in = new StubInput(
+                new String[]{"0", "1", "Item name edited", "1"}
+        );
+        UserAction[] actions = {
+                new ReplaceAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertEquals(tracker.findAll()[0].getName(), replaced);
     }
 
     @Test
     public void whenDeleteItem() {
         Tracker tracker = new Tracker();
-        Item item = new Item("deleted item");
-        tracker.add(item);
-        String[] answers = {String.valueOf(item.getId())};
-        DeleteAction deleteAction = new DeleteAction();
-        deleteAction.execute(new StubInput(answers), tracker);
-        Item deleted = tracker.findById(item.getId());
-        assertNull(deleted);
+        Item item = tracker.add(new Item("Deleted item"));
+        Input in = new StubInput(
+                new String[]{"0", "1", "1"}
+        );
+        UserAction[] actions = {
+                new DeleteAction(),
+                new ExitAction()
+        };
+        new StartUI().init(in, tracker, actions);
+        assertNull(tracker.findById(item.getId()));
     }
 
     @Test
