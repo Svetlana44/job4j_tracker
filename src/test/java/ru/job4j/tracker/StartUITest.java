7a25376a7d45bc88f5398847d34db02d217d7d1b
SpS <shchaaas@mail.ru>
2022-05-13 21:31:25 +0300

    9.2. Тесты вывода на консоль [#33585]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -2,10 +2,125 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 public class StartUITest {
+    @Test
+    public void whenReplaceItemTestOutputIsSuccessfully() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        String replaceName = "New Test Name";
+        Input in = new StubInput(
+                new String[]{"0", String.valueOf(one.getId()), replaceName, "1"}
+        );
+        UserAction[] actions = new UserAction[]{
+                new ReplaceAction(out),
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertEquals(out.toString(),
+                "Menu." + ln
+                        + "0. Edit item" + ln
+                        + "1. Exit Program" + ln
+                        + "=== Edit item ===" + ln
+                        + "Заявка изменена успешно." + ln
+                        + "Menu." + ln
+                        + "0. Edit item" + ln
+                        + "1. Exit Program" + ln
+                        + "App is closed." + ln);
+    }
+
+    @Test
+    public void whenFindByNameTestOutputIsSuccessfully() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        Input in = new StubInput(
+                new String[]{"0", "test1", "1"});
+        UserAction[] actions = new UserAction[]{
+                new FindItemsByNameAction(out),
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        LocalDateTime localDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        assertEquals(out.toString(), "Menu." + ln
+                + "0. Find items by name" + ln
+                + "1. Exit Program" + ln
+                + "=== Find items by name ===" + ln
+                + "Item{id=1, name='test1', created="
+                + localDateTime.format(formatter) + "}" + ln
+                + "Menu." + ln
+                + "0. Find items by name" + ln
+                + "1. Exit Program" + ln
+                + "App is closed." + ln
+        );
+    }
+
+    @Test
+    public void whenFindByIdTestOutputIsSuccessfully() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        Input in = new StubInput(
+                new String[]{"0", String.valueOf(one.getId()), "1"});
+        UserAction[] actions = new UserAction[]{
+                new FindItemByIdAction(out),
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        LocalDateTime localDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        assertEquals(out.toString(), "Menu." + ln
+                + "0. Find item by id" + ln
+                + "1. Exit Program" + ln
+                + "=== Find item by id ===" + ln
+                + "Item{id=1, name='test1', created="
+                + localDateTime.format(formatter) + "}" + ln
+                + "Menu." + ln
+                + "0. Find item by id" + ln
+                + "1. Exit Program" + ln
+                + "App is closed." + ln
+
+        );
+
+    }
+
+    @Test
+    public void whenFindAllTestOutputIsSuccessfully() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        Input in = new StubInput(
+                new String[]{"0", "1"});
+        UserAction[] actions = new UserAction[]{
+                new ShowAllItemAction(out),
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        LocalDateTime localDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        assertEquals(out.toString(), "Menu." + ln
+                + "0. Show all items" + ln
+                + "1. Exit Program" + ln
+                + "=== Show all items ===" + ln
+                + "Item{id=1, name='test1', created="
+                + localDateTime.format(formatter) + "}" + ln
+                + "Menu." + ln
+                + "0. Show all items" + ln
+                + "1. Exit Program" + ln
+                + "App is closed." + ln
+        );
+    }
 
     @Test
     public void whenEditItem() {
