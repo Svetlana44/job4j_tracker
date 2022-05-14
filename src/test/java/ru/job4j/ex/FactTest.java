19ae9dbf50efcc30200da473ecf17e6fd19afc5d
SpS <shchaaas@mail.ru>
2022-05-14 22:09:12 +0300

    0.3. Тестирование исключений с junit [#219366 #300725] v2

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -6,13 +6,13 @@ import static org.junit.Assert.*;
 
 public class FactTest {
     @Test(expected = IllegalArgumentException.class)
-    public void calcShouldThrowIllegalArgumentExeption_whenInputLessNull() {
+    public void calcShouldThrowIllegalArgumentExeptionWhenInputLessNull() {
         Fact fact = new Fact();
         fact.calc(-1);
     }
 
     @Test
-    public void calcShouldThrowExeption_whenInputLessNull() {
+    public void calcShouldThrowExeptionWhenInputLessNull() {
         Fact fact = new Fact();
         assertThrows(IllegalArgumentException.class, () -> fact.calc(-1));
     }
