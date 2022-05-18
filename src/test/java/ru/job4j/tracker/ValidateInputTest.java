85db3222ca7895271358e9bc15dcb36d4b29a565
SpS <shchaaas@mail.ru>
2022-05-18 21:46:29 +0300

    2.1. Тест на ValidateInput [#311887]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -9,7 +9,7 @@ public class ValidateInputTest {
     public void whenInvalidInput() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[]{"one", "2"}
+                new String[]{"one", "1"}
         );
         ValidateInput input = new ValidateInput(out, in);
         int selected = input.askInt("Enter menu:");
