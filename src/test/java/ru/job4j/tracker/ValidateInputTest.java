d5d36a0640d5ebd494de077d0ef780e7eaf93370
SpS <shchaaas@mail.ru>
2022-05-18 21:44:55 +0300

    2.1. Тест на ValidateInput [#311887]

---
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ValidateInputTest {
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[]{"one", "2"}
+        );
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertEquals(selected, 1);
+    }
+
+    @Test
+    public void whenValidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[]{"3"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertEquals(selected, 3);
+    }
+
+    @Test
+    public void whenSomeValid() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[]{"1", "2", "3"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertEquals(selected, 1);
+    }
+
+    @Test
+    public void whenMinus() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[]{"-1"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertEquals(selected, -1);
+    }
+}
