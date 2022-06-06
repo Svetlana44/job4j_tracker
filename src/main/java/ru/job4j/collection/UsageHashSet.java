06192cf8afb9d9b5100fa0cdfca5d1e3f500d6e0
SpS <shchaaas@mail.ru>
2022-06-06 21:16:31 +0300

    0. Set, HashSet, Iterator. [#10095 #309033] v2

--- src/main/java/ru/job4j/collection/UsageHashSet.java
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -9,6 +9,14 @@ public class UsageHashSet {
         autos.add("BMW");
         autos.add("Volvo");
         autos.add("Toyota");
+        autos.add("Lada");
+        autos.add("BMW");
+        autos.add("Volvo");
+        autos.add("Toyota");
+        autos.add("Lada");
+        autos.add("BMW");
+        autos.add("Volvo");
+        autos.add("Toyota");
         for (String auto : autos) {
             System.out.println(auto);
         }
