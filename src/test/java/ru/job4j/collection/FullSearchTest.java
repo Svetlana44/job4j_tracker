d625230b5712656d86b4f394cb6131ffe33399dc
SpS <shchaaas@mail.ru>
2022-06-06 21:31:30 +0300

    1. Уникальные задачи. [#212653]

---
+++ src/test/java/ru/job4j/collection/FullSearchTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+public class FullSearchTest {
+
+    @Test
+    public void extractNumber() {
+        List<Task> tasks = Arrays.asList(
+                new Task("1", "First desc"),
+                new Task("2", "Second desc"),
+                new Task("1", "First desc")
+        );
+        Set<String> expected = new HashSet<>(Arrays.asList("1", "2"));
+        assertEquals(FullSearch.extractNumber(tasks), expected);
+    }
+}
