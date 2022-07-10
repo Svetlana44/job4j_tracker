8cc3eb9997d952dcc2627e5c4a97b0faed5f4635
SpS <shchaaas@mail.ru>
2022-07-10 17:52:30 +0300

    2. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,29 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+    public static List<String> fillGaps(List<String> deps) {
+        Set<String> tmp = new LinkedHashSet<>();
+        for (String value : deps) {
+            String start = "";
+            for (String el : value.split("/")) {
+                if (start == "") {
+                    tmp.add(el);
+                    start += el;
+                } else {
+                    tmp.add(start + "/" + el);
+                }
+            }
+        }
+        return new ArrayList<>(tmp);
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        orgs.sort(Comparator.naturalOrder());
+    }
+
+    public static void sortDesc(List<String> orgs) {
+        orgs.sort(new DepDescComp());
+    }
+}
---
+++ src/test/java/ru/job4j/collection/DepDescCompTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DepDescCompTest {
+
+    @Test
+    public void compare() {
+        int rsl = new DepDescComp().compare(
+                "K2/SK1/SSK2",
+                "K2/SK1/SSK1"
+        );
+        assertTrue(rsl > 0);
+    }
+
+    @Test
+    public void whenUpDepartmentGoBefore() {
+        int rsl = new DepDescComp().compare(
+                "K2",
+                "K2/SK1"
+        );
+        assertTrue(rsl < 0);
+    }
+
+    @Test
+    public void whenFirstDepartmentIsDifferentThenAsc() {
+        int rsl = new DepDescComp().compare(
+                "K2/SK1/SSK1",
+                "K1/SK1/SSK2"
+        );
+        assertTrue(rsl < 0);
+    }
+
+    @Test
+    public void whenFirstDepartmentIsDifferentThenDesc() {
+        int rsl = new DepDescComp().compare(
+                "K1/SK1/SSK1",
+                "K2/SK1"
+        );
+        assertTrue(rsl > 0);
+    }
+
+    @Test
+    public void myCompareFirstDepartmentIsEqualsThenZero() {
+        int rsl = new DepDescComp().compare(
+                "K2/SK1",
+                "K2/SK1"
+        );
+        assertTrue(rsl == 0);
+    }
+}
