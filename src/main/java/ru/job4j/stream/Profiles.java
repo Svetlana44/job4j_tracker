fdc27ceefda729ec4923b4fc1d98f85bee29e45d
SpS <shchaaas@mail.ru>
2022-08-21 21:45:06 +0300

    2. Список адресов [#110062]

---
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Profiles {
+
+    public static List<Address> collect(List<Profile> profiles) {
+        return profiles.stream()
+                .flatMap(Stream::ofNullable)
+                .map(Profile::getAddress)
+                .collect(Collectors.toList());
+    }
+}
