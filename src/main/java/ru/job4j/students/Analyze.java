652fdaa832bb44d38d7015d2c93730153d0e9ad2
SpS <shchaaas@mail.ru>
2022-10-13 20:49:48 +0300

    2. Аттестация [#49777]v2

--- src/main/java/ru/job4j/students/Analyze.java
+++ src/main/java/ru/job4j/students/Analyze.java
@@ -1,6 +1,5 @@
 package ru.job4j.students;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,58 +7,51 @@ import java.util.stream.Stream;
 
 public class Analyze {
     public static double averageScore(Stream<Pupil> stream) {
-        return stream.flatMapToLong(pupil -> pupil.subjects().stream().mapToLong(subject -> subject.score())).average().getAsDouble();
+        return stream
+                .flatMapToLong(pupil -> pupil.subjects()
+                        .stream()
+                        .mapToLong(Subject::score))
+                .average()
+                .getAsDouble();
     }
 
     public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
-        List<Tuple> tuples = new ArrayList<>();
-        stream.flatMap(pupil -> pupil.subjects().stream().map(subject -> new Tuple(pupil.name(), subject.score())))
-                .collect(Collectors.toList())
+        return stream.flatMap(pupil -> pupil.subjects().stream().map(subject -> new Tuple(pupil.name(), subject.score()))).toList()
                 .stream()
-                .collect(Collectors.groupingBy(tuple -> tuple.name(), Collectors.averagingDouble(t -> t.score())))
-                .forEach((k, v) -> tuples.add(new Tuple((String) k, (Double) v)));
-
-        return tuples;
+                .collect(Collectors.groupingBy(Tuple::name, Collectors.averagingDouble(Tuple::score)))
+                .entrySet().stream()
+                .map(entry -> new Tuple(entry.getKey(), entry.getValue()))
+                .collect(Collectors.toList());
     }
 
     public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
-        List<Tuple> tuples = new ArrayList<>();
-        stream.flatMap(pupil -> pupil.subjects().stream().map(subject -> new Tuple(subject.name(), subject.score())))
+        return stream.flatMap(pupil -> pupil.subjects().stream().map(subject -> new Tuple(subject.name(), subject.score())))
                 .collect(Collectors.toList())
                 .stream()
-                .collect(Collectors.groupingBy(tuple -> tuple.name(), Collectors.averagingDouble(t -> t.score())))
-                .forEach((k, v) -> tuples.add(new Tuple((String) k, (Double) v)));
-
-        return tuples;
+                .collect(Collectors.groupingBy(Tuple::name, Collectors.averagingDouble(Tuple::score)))
+                .entrySet().stream()
+                .map(entry -> new Tuple(entry.getKey(), entry.getValue()))
+                .collect(Collectors.toList());
     }
 
     public static Tuple bestStudent(Stream<Pupil> stream) {
-        List<Tuple> tuples = maxScoreByPupil(stream);
-        return tuples.stream().max(Comparator.comparingDouble(t -> t.score())).get();
+        return stream.flatMap(pupil -> pupil.subjects().stream().map(subject -> new Tuple(pupil.name(), subject.score()))).toList()
+                .stream()
+                .collect(Collectors.groupingBy(Tuple::name, Collectors.summingDouble(Tuple::score)))
+                .entrySet().stream()
+                .map(entry -> new Tuple(entry.getKey(), entry.getValue()))
+                .collect(Collectors.toList())
+                .stream().max(Comparator.comparingDouble(Tuple::score)).get();
     }
 
     public static Tuple bestSubject(Stream<Pupil> stream) {
-
-        List<Tuple> tuples = new ArrayList<>();
-        stream.flatMap(pupil -> pupil.subjects().stream().map(subject -> new Tuple(subject.name(), subject.score())))
+        return stream.flatMap(pupil -> pupil.subjects().stream().map(subject -> new Tuple(subject.name(), subject.score())))
                 .collect(Collectors.toList())
                 .stream()
-                .collect(Collectors.groupingBy(tuple -> tuple.name(), Collectors.summingDouble(t -> t.score())))
-                .forEach((k, v) -> tuples.add(new Tuple((String) k, (Double) v)));
-        return tuples.stream().max(Comparator.comparingDouble(t -> t.score())).get();
-    }
-
-    /*
-    my method summing score of student by subject
-     */
-    public static List<Tuple> maxScoreByPupil(Stream<Pupil> stream) {
-        List<Tuple> tuples = new ArrayList<>();
-        stream.flatMap(pupil -> pupil.subjects().stream().map(subject -> new Tuple(pupil.name(), subject.score())))
+                .collect(Collectors.groupingBy(Tuple::name, Collectors.summingDouble(Tuple::score)))
+                .entrySet().stream()
+                .map(entry -> new Tuple(entry.getKey(), entry.getValue()))
                 .collect(Collectors.toList())
-                .stream()
-                .collect(Collectors.groupingBy(tuple -> tuple.name(), Collectors.summingDouble(t -> t.score())))
-                .forEach((k, v) -> tuples.add(new Tuple((String) k, (Double) v)));
-
-        return tuples;
+                .stream().max(Comparator.comparingDouble(Tuple::score)).get();
     }
 }
