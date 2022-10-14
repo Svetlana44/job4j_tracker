70ac8eb91ff2bf54a445737bf60863e1999d579f
SpS <shchaaas@mail.ru>
2022-10-14 19:55:36 +0300

    2. Аттестация [#49777]v3

--- src/main/java/ru/job4j/students/Analyze.java
+++ src/main/java/ru/job4j/students/Analyze.java
@@ -41,7 +41,7 @@ public class Analyze {
                 .entrySet().stream()
                 .map(entry -> new Tuple(entry.getKey(), entry.getValue()))
                 .collect(Collectors.toList())
-                .stream().max(Comparator.comparingDouble(Tuple::score)).get();
+                .stream().max(Comparator.comparingDouble(Tuple::score)).orElse(new Tuple("", 0.0));
     }
 
     public static Tuple bestSubject(Stream<Pupil> stream) {
@@ -52,6 +52,6 @@ public class Analyze {
                 .entrySet().stream()
                 .map(entry -> new Tuple(entry.getKey(), entry.getValue()))
                 .collect(Collectors.toList())
-                .stream().max(Comparator.comparingDouble(Tuple::score)).get();
+                .stream().max(Comparator.comparingDouble(Tuple::score)).orElse(new Tuple("", 0.0));
     }
 }
