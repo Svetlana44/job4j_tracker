3e05fd955902eac9f1cae7ed97e160a8c42ebc7f
SpS <shchaaas@mail.ru>
2022-12-04 20:10:43 +0300

    4. Ссылки на конструкторы [#504922]

---
+++ src/main/java/ru/job4j/lambda/FuncInterface.java
@@ -0,0 +1,9 @@
+/*
+берёт строку, возвращает модель
+ */
+
+package ru.job4j.lambda;
+
+public interface FuncInterface {
+    Model function(String s);
+}
---
+++ src/main/java/ru/job4j/lambda/Model.java
@@ -0,0 +1,16 @@
+package ru.job4j.lambda;
+
+public class Model {
+    private String name;
+
+    Model() {
+    }
+
+    Model(String str) {
+        name = str;
+    }
+
+    String getName() {
+        return name;
+    }
+}
